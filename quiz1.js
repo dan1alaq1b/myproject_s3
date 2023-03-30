24b3fa4ed024fc8302105bafa93dd0abffd212f2
Muhd Danial <m.danial.aqib@gmail.com>
2023-03-30 22:31:25 +0800

    revised

--- quiz1.js
+++ quiz1.js
@@ -6,11 +6,11 @@ console.log(message, typeof message)
 
 
 
+let str = "1.258"
+let num = "1"
 
-
-
-
-
+let total = parseFloat(str)+num
+console.log(total, typeof total)
 
 let data = ["benr", 3523, true]
 
@@ -26,3 +26,20 @@ let user = {
 }
 console.log(user.name)
 console.log(user.age)
+console.log(user.faculty)
+
+let messages = [
+    {
+        name: "usr1",
+        date: "1/4/2023",
+        msg: "very good video",
+        like: 1000    
+    },
+    {
+        name: "usr2",
+        date: "2/4/2023",
+        msg: "very good video",
+        like: 3000    
+    }//,
+]
+console.log(messages[1].msg) // >> very good video
