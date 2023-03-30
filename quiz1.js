fc13e22de9ff41e85a15b8329861c8ffdff25951
Muhd Danial <m.danial.aqib@gmail.com>
2023-03-30 15:52:36 +0800

    message

---
+++ quiz1.js
@@ -0,0 +1,28 @@
+let message = "benr"
+console.log(message, typeof message)
+
+
+
+
+
+
+
+
+
+
+
+
+let data = ["benr", 3523, true]
+
+console.log(data[0])
+console.log(data[1])
+console.log(data[2])
+console.log(data[3])
+
+let user = {
+    name: "danial",
+    age: 21,
+    faculty: "FKEKK"
+}
+console.log(user.name)
+console.log(user.age)
